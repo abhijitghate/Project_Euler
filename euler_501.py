af1e37867f890f59c7f59ad0ac1d4b9e855838e3
abhijitghate <abhijitsghate0605@gmail.com>
2018-05-20 11:58:02 +0530

    incomplete

---
+++ euler_501.py
@@ -0,0 +1,20 @@
+
+answer_dict = {}
+
+def num_divisor(x):
+	divisors = []
+	for i in range(1,x+1):
+		if x%i == 0:
+			divisors.append(i)
+	answer_dict.update({x:divisors})
+
+
+count = 0 
+for i in xrange(1,10**12):
+	num_divisor(i)
+
+for i in answer_dict:
+	if len(answer_dict[i]) == 8:
+		count = count + 1
+print count
+
